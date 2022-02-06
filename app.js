011fe6d8221f30163ff2f1b009408601c7ccd1d0
MKougia <kougia.mihkel@khk.ee>
2022-02-06 17:22:39 +0200

    Displayed city weather data from API

--- app.js
+++ app.js
@@ -8,12 +8,22 @@ function weatherDataFetch( city ) {
 	}) // Convert data to json
 	.then(function(data) {
 		console.log(data);
+		drawWeather(data);
 	})
 	.catch(function() {
 		// catch any errors
-	})
+	});
 }
 
 function cityWeather(e) {
 	weatherDataFetch( 'Tallinn' );
+}
+
+function drawWeather( data ) {
+	var celsius = Math.round(parseFloat(data.main.temp)-273.15);
+	var description = data.weather[0].description;
+
+	document.querySelector('#description').innerHTML = description;
+	document.querySelector('#temp').innerHTML = celsius + '&deg;';
+	document.querySelector('#location').innerHTML = data.name;
 }
